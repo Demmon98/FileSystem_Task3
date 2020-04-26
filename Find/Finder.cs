48a29e2226cc1009df25c6797978e3ef02490086
Demmon <dima.521.shvab@gmail.com>
2020-04-26 22:02:19 +0300

    Добавьте файлы проекта.

---
+++ Find/Finder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find
+{
+    class Finder
+    {
+        List<DirectoryInfo> pathes = new List<DirectoryInfo>();
+        int spaceCount = 0;
+        string separatedString = "-+";
+        string fullPath = "";
+
+        public Finder() => spaceCount = 0;
+
+        public Finder(int tabCount, string fullPath)
+        {
+            this.spaceCount = tabCount;
+            this.fullPath = fullPath;
+        }
+
+        public int Find(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                DirectoryInfo directory = new DirectoryInfo(path);
+
+                Console.WriteLine(new string(' ', spaceCount) + separatedString + fullPath + "\\" + (fullPath == "" ? directory.Name : fullPath));
+
+                if (string.IsNullOrEmpty(fullPath))
+                    fullPath = directory.Name;
+
+                foreach (var item in directory.GetFiles())
+                {
+                    Console.WriteLine(new string(' ', spaceCount + 2) + separatedString + fullPath + "\\" + item.Name);
+                }
+
+                pathes.AddRange(directory.GetDirectories());
+
+                foreach (var item in pathes)
+                {
+                    new Finder(spaceCount + 2, fullPath + "\\" + item.Name).Find(item.FullName);
+                }
+
+                return 0;
+            }
+            else
+                Console.WriteLine("Directory not exist");
+
+            return -1;
+        }
+    }
+}
---
+++ Find/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input path to directory (. - current directory)");
+
+            int res;
+
+            do
+            {
+                string path = Console.ReadLine();
+                res = new Finder().Find(path);
+            }
+            while (res != 0);
+
+            Console.ReadKey();
+        }
+    }
+}
